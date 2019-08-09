782a5ddbf61c46208c1ca4ae3bfcc43357ecf2eb
chris13012 <christophecaillet@icloud.com>
2019-08-09 15:55:45 +0200

    Updates

---
+++ precache-manifest.bebc9f33a1eb299499cb2cd407da2c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bba62c9d206db756db5c1b142ee684a",
+    "url": "/emojiSearch/index.html"
+  },
+  {
+    "revision": "27bb3d85e9129d0775f0",
+    "url": "/emojiSearch/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cf015d4cec3cc35f04cc",
+    "url": "/emojiSearch/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "27bb3d85e9129d0775f0",
+    "url": "/emojiSearch/static/js/2.6dec9a9b.chunk.js"
+  },
+  {
+    "revision": "cf015d4cec3cc35f04cc",
+    "url": "/emojiSearch/static/js/main.f275192c.chunk.js"
+  },
+  {
+    "revision": "409098e34972aad31be8",
+    "url": "/emojiSearch/static/js/runtime~main.916b5353.js"
+  }
+]);
